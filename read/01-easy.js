02a6a5e1a70618ed0d36f7352c30da1ef26c92ae
bubbles linux <appenzeller.barbara@gmail.com>
2021-07-22 15:51:01 +0100

    Read: equal Arr

--- read/01-easy.js
+++ read/01-easy.js
@@ -2,3 +2,20 @@
 [true, false, true, false].slice(2000000, 3000000); // []
 
 [30, 200, 1000].sort(); // [1000, 200, 30]
+
+/*Read
+What does this function do? 
+What type of value does it return? 
+What would be a good name for this function 
+Write a function call with examples for arr1 & arr2
+*/
+
+function f(arr1, arr2) {
+	let result = arr1.length === arr2.length;
+	arr1.forEach((value, i) => {
+		if (value !== arr2[i]) {
+			result = false;
+		}
+	});
+	return result;
+}
